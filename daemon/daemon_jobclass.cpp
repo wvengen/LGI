317ccebcd3c92b5df7c6d59f8f122d07ba58601e
Mark Somers <m.somers@chem.leidenuniv.nl>
2007-08-15 15:56:37 +0000

    git-svn-id: https://gliteui.wks.gorlaeus.net/svn/LGI/trunk@68 ce53c53d-4636-0410-80b1-9429a1ee4427

--- daemon/daemon_jobclass.cpp
+++ daemon/daemon_jobclass.cpp
@@ -34,14 +34,39 @@ DaemonJob::DaemonJob( string TheJobDirectory )
 
  DIR *Entry = opendir( TheJobDirectory.c_str() );
 
- if( Entry != NULL )
+ if( Entry == NULL )
  {
-  closedir( Entry );
-  JobDirectory = TheJobDirectory;
-  NORMAL_LOG( "DaemonJob::DaemonJob; Set up job with JobDirectory=" << TheJobDirectory );
+  CRITICAL_LOG( "DaemonJob::DaemonJob; Could not open and read from JobDirectory=" << TheJobDirectory );
+  return;
  }
  else
-  CRITICAL_LOG( "DaemonJob::DaemonJob; Could not open and read from JobDirectory=" << TheJobDirectory );
+  closedir( Entry );
+
+ // now check if some critical files should be present and untouched...
+
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_PROJECT_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_PROJECT_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_THIS_PROJECT_SERVER_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_THIS_PROJECT_SERVER_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_PROJECT_MASTER_SERVER_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_PROJECT_MASTER_SERVER_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_APPLICATION_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_APPLICATION_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_ID_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_ID_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_OWNERS_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_OWNERS_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_READ_ACCESS_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_READ_ACCESS_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_STATE_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_STATE_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_STATE_TIME_STAMP_FILE ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_STATE_TIME_STAMP_FILE << " seems corrupt in " << TheJobDirectory ); return; }
+
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_CHECK_LIMITS_SCRIPT ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_CHECK_LIMITS_SCRIPT << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_CHECK_RUNNING_SCRIPT ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_CHECK_RUNNING_SCRIPT << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_CHECK_FINISHED_SCRIPT ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_CHECK_FINISHED_SCRIPT << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_PROLOGUE_SCRIPT ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_PROLOGUE_SCRIPT << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_EPILOGUE_SCRIPT ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_EPILOGUE_SCRIPT << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_RUN_SCRIPT ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_RUN_SCRIPT << " seems corrupt in " << TheJobDirectory ); return; }
+ if( ReadStringFromFile( TheJobDirectory + "/" + LGI_JOBDAEMON_JOB_ABORT_SCRIPT ).empty() ) { CRITICAL_LOG( "DaemonJob::DaemonJob; File " << LGI_JOBDAEMON_JOB_ABORT_SCRIPT << " seems corrupt in " << TheJobDirectory ); return; }
+
+ // all files are there and are okay... accept the job directory...
+
+ JobDirectory = TheJobDirectory;
+
+ NORMAL_LOG( "DaemonJob::DaemonJob; Set up job with JobDirectory=" << TheJobDirectory );
 }
 
 // -----------------------------------------------------------------------------
@@ -54,7 +79,21 @@ DaemonJob::DaemonJob( string TheXML, DaemonConfig TheConfig, int ProjectNumber, 
 
  JobDirectory.clear();
 
- // first check if run dir is readable...
+ // first check if config matches the XML response
+
+ if( NormalizeString( Parse_XML( TheXML, "project" ) ) != TheConfig.Project( ProjectNumber ).Project_Name() )
+ {
+  CRITICAL_LOG( "DaemonJob::DaemonJob; The XML response project does not match the one specified in the config" );
+  return;
+ }
+
+ if( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "application" ) ) != TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Application_Name() )
+ {
+  CRITICAL_LOG( "DaemonJob::DaemonJob; The XML response application does not match the one specified in the config" );
+  return;
+ }
+
+ // then check if run dir is readable...
 
  DIR *Entry = opendir( TheConfig.RunDirectory().c_str() );
 
@@ -84,7 +123,31 @@ DaemonJob::DaemonJob( string TheXML, DaemonConfig TheConfig, int ProjectNumber, 
   return;
  }
 
- NORMAL_LOG( "DaemonJob::DaemonJob; Set up job with JobDirectory=" << JobDirectory );
+ // then start writing stuff to disk from the response...
+
+ WriteStringToFile( NormalizeString( Parse_XML( TheXML, "project" ) ), JobDirectory + "/" + LGI_JOBDAEMON_PROJECT_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( TheXML, "this_project_server" ) ), JobDirectory + "/" + LGI_JOBDAEMON_THIS_PROJECT_SERVER_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( TheXML, "project_master_server" ) ), JobDirectory + "/" + LGI_JOBDAEMON_PROJECT_MASTER_SERVER_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "application" ) ), JobDirectory + "/" + LGI_JOBDAEMON_APPLICATION_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "job_id" ) ), JobDirectory + "/" + LGI_JOBDAEMON_JOB_ID_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "owners" ) ), JobDirectory + "/" + LGI_JOBDAEMON_OWNERS_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "read_access" ) ), JobDirectory + "/" + LGI_JOBDAEMON_READ_ACCESS_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "job_specifics" ) ), JobDirectory + "/" + LGI_JOBDAEMON_JOB_SPECIFICS_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "input" ) ), JobDirectory + "/" + LGI_JOBDAEMON_INPUT_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "state" ) ), JobDirectory + "/" + LGI_JOBDAEMON_STATE_FILE );
+ WriteStringToFile( NormalizeString( Parse_XML( Parse_XML( TheXML, "job" ), "state_time_stamp" ) ), JobDirectory + "/" + LGI_JOBDAEMON_STATE_TIME_STAMP_FILE );
+ 
+ // finally also dump the scripts there...
+
+ WriteStringToFile( TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Job_Check_Limits_Script() , JobDirectory + "/" + LGI_JOBDAEMON_JOB_CHECK_LIMITS_SCRIPT );
+ WriteStringToFile( TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Job_Check_Running_Script() , JobDirectory + "/" + LGI_JOBDAEMON_JOB_CHECK_RUNNING_SCRIPT );
+ WriteStringToFile( TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Job_Check_Finished_Script() , JobDirectory + "/" + LGI_JOBDAEMON_JOB_CHECK_FINISHED_SCRIPT );
+ WriteStringToFile( TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Job_Prologue_Script() , JobDirectory + "/" + LGI_JOBDAEMON_JOB_PROLOGUE_SCRIPT );
+ WriteStringToFile( TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Job_Run_Script() , JobDirectory + "/" + LGI_JOBDAEMON_JOB_RUN_SCRIPT );
+ WriteStringToFile( TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Job_Epilogue_Script() , JobDirectory + "/" + LGI_JOBDAEMON_JOB_EPILOGUE_SCRIPT );
+ WriteStringToFile( TheConfig.Project( ProjectNumber ).Application( ApplicationNumber ).Job_Abort_Script() , JobDirectory + "/" + LGI_JOBDAEMON_JOB_ABORT_SCRIPT );
+
+ NORMAL_LOG( "DaemonJob::DaemonJob; Job with JobDirectory=" << JobDirectory << " has been setup" );
 }
 
 // -----------------------------------------------------------------------------
@@ -92,7 +155,8 @@ DaemonJob::DaemonJob( string TheXML, DaemonConfig TheConfig, int ProjectNumber, 
 string DaemonJob::ReadStringFromFile( string FileName )
 {
  fstream File( FileName.c_str(), fstream::in );
- string Buffer, Line;
+ fstream HashFile( ( FileName + HASHFILE_EXTENTION ).c_str(), fstream::in );
+ string Buffer, Line, TheHash;
 
  Buffer.reserve( 1024 );
 
@@ -107,6 +171,16 @@ string DaemonJob::ReadStringFromFile( string FileName )
   Buffer = Buffer.substr( 0, Buffer.length() - 2 );
  else
   Buffer.clear();
+
+ BinHex( Hash( Buffer ), TheHash );
+
+ getline( HashFile, Line );
+
+ if( Line != TheHash )
+ {
+  Buffer.clear();
+  CRITICAL_LOG_RETURN( Buffer, "DaemonJob::ReadStringFromFile; Data read from file " << FileName << " does not match hash" );
+ }
  
  DEBUG_LOG_RETURN( Buffer, "DaemonJob::ReadStringFromFile; Data returned from file " << FileName << ":  " << Buffer );
 }
@@ -116,8 +190,13 @@ string DaemonJob::ReadStringFromFile( string FileName )
 void DaemonJob::WriteStringToFile( string String, string FileName )
 {
  fstream File( FileName.c_str(), fstream::out );
+ fstream HashFile( ( FileName + HASHFILE_EXTENTION ).c_str(), fstream::out );
+ string  TheHash;
+
+ BinHex( Hash( String ), TheHash );
 
  File << String;
+ HashFile << TheHash;
 }
 
 // -----------------------------------------------------------------------------
--- daemon/daemon_jobclass.h
+++ daemon/daemon_jobclass.h
@@ -40,6 +40,31 @@
 
 using namespace std;
 
+// ------------ some handy defines -------------------------------------------
+
+#define HASHFILE_EXTENTION                         ".hash"
+
+#define LGI_JOBDAEMON_PROJECT_FILE                 "LGI_project"
+#define LGI_JOBDAEMON_THIS_PROJECT_SERVER_FILE     "LGI_this_project_server"
+#define LGI_JOBDAEMON_PROJECT_MASTER_SERVER_FILE   "LGI_project_master_server"
+#define LGI_JOBDAEMON_APPLICATION_FILE             "LGI_application"
+#define LGI_JOBDAEMON_JOB_ID_FILE                  "LGI_job_id"
+#define LGI_JOBDAEMON_OWNERS_FILE                  "LGI_owners"
+#define LGI_JOBDAEMON_READ_ACCESS_FILE             "LGI_read_access"
+#define LGI_JOBDAEMON_JOB_SPECIFICS_FILE           "LGI_job_specifics"
+#define LGI_JOBDAEMON_INPUT_FILE                   "LGI_input"
+#define LGI_JOBDAEMON_OUTPUT_FILE                  "LGI_output"
+#define LGI_JOBDAEMON_STATE_FILE                   "LGI_state"
+#define LGI_JOBDAEMON_STATE_TIME_STAMP_FILE        "LGI_state_time_stamp"
+
+#define LGI_JOBDAEMON_JOB_CHECK_LIMITS_SCRIPT      "LGI_job_check_limits_script"
+#define LGI_JOBDAEMON_JOB_CHECK_RUNNING_SCRIPT     "LGI_job_check_running_script"
+#define LGI_JOBDAEMON_JOB_CHECK_FINISHED_SCRIPT    "LGI_job_check_finished_script"
+#define LGI_JOBDAEMON_JOB_PROLOGUE_SCRIPT          "LGI_job_prologue_script"
+#define LGI_JOBDAEMON_JOB_RUN_SCRIPT               "LGI_job_run_script"
+#define LGI_JOBDAEMON_JOB_EPILOGUE_SCRIPT          "LGI_job_epilogue_script"
+#define LGI_JOBDAEMON_JOB_ABORT_SCRIPT             "LGI_job_abort_script"
+
 // -----------------------------------------------------------------------------
 
 class DaemonJob
