217ce708efef49bb785bdfca779ce56dc03fcb32
Mark Somers <m.somers@chem.leidenuniv.nl>
2007-08-25 20:48:56 +0000

    Updated a possible signup deadlock...
    git-svn-id: https://gliteui.wks.gorlaeus.net/svn/LGI/trunk@149 ce53c53d-4636-0410-80b1-9429a1ee4427

--- resources/resource_signup_resource.php
+++ resources/resource_signup_resource.php
@@ -15,8 +15,8 @@ global $ErrorMsgs;
 $ResourceData = Resource_Verify( $_POST[ "project" ] );      
 
 // check if this call is valid...
-if( $ResourceData->active )
-  return( LGI_Error_Response( 8, $ErrorMsgs[ 8 ], "" ) );
+// if( $ResourceData->active )
+//  return( LGI_Error_Response( 8, $ErrorMsgs[ 8 ], "" ) );
 
 // remove any stale locks this resource might have...
 Resource_Remove_Stale_Locks( $ResourceData );
