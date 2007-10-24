4e9460a70892cea7ccae35f30730f68cec474be3
Mark Somers <m.somers@chem.leidenuniv.nl>
2007-10-24 11:12:50 +0000

    Updated basic interface qstat code...
    git-svn-id: https://gliteui.wks.gorlaeus.net/svn/LGI/trunk@324 ce53c53d-4636-0410-80b1-9429a1ee4427

--- basic_interface/qstat.php
+++ basic_interface/qstat.php
@@ -19,12 +19,15 @@
 // http://www.gnu.org/licenses/gpl.txt
 
 require_once( '../inc/Config.inc' );
+require_once( '../inc/Errors.inc' );
 require_once( '../inc/Interfaces.inc' );
 require_once( '../inc/Utils.inc' );
 require_once( '../inc/Html.inc' );
 
 global $Config;
 global $ErrorMsgs;
+
+Page_Head();
 
 // check if user is set in request... or use value from certificate...
 $CommonNameArray = CommaSeparatedField2Array( SSL_Get_Common_Name(), ";" );
@@ -54,16 +57,126 @@ else
   $Project = $_POST[ "project" ];
 $Project = mysql_escape_string( $Project );
 
-// now verfiy the user using the basic browser interface...
+// now verfiy the user using the basic browser interface... also make MySQL connection...
 $ErrorCode = Interface_Verify( $Project, $User, $Groups, false );
+if( $ErrorCode !== 0 ) Exit_With_Text( "ERROR: ".$ErrorMsgs[ $ErrorCode ] );
 
-Page_Head();
-Start_Table();
-Row1( "LGI queue at ".time() );
-Row2( "User:", $User ); 
-Row2( "Groups:", $Groups ); 
-Row2( "Project:", $Project ); 
-End_Table();
+// check if job_id is set in request... otherwise default to list mode...
+if( isset( $_GET[ "job_id" ] ) )
+ $Job_ID = $_GET[ "job_id" ];
+else
+ if( isset( $_POST[ "job_id" ] ) )
+  $Job_ID = $_POST[ "job_id" ];
+
+if( isset( $Job_ID ) )               // we requested details on a job...
+{
+ if( !is_numeric( $Job_ID ) ) Exit_With_Text( "ERROR: Job_id not a number" );
+
+ $JobSpecs = Interface_Wait_For_Cleared_Spin_Lock_On_Job( $Job_ID, false );
+
+ if( $JobSpecs === 15 ) Exit_With_Text( "ERROR: Job was not found in database" );
+ if( $JobSpecs === 2 ) Exit_With_Text( "ERROR: Server too busy, backoff for a minute!" );
+
+ if( !Interface_Is_User_Allowed_To_Read_Job( $JobSpecs, $User, $Groups ) ) Exit_With_Text( "ERROR: User is not allowed to get details of this job" );
+
+ Start_Table();
+ Row1( "<center><font color='green' size='4'><b>Leiden Grid Infrastructure basic interface QSTAT at ".gmdate( "j M Y G:i", time() )." UTC of job with ID=$Job_ID</b></font></center>" );
+ Row2( "<b>This project server:</b>", Get_Server_URL() ); 
+ Row2( "<b>Project master server:</b>", Get_Master_Server_URL() ); 
+ Row2( "<b>User:</b>", $User ); 
+ Row2( "<b>Groups:</b>", $Groups ); 
+ Row2( "<b>Project:</b>", $Project ); 
+ Row1( "<center><font color='green' size='4'><b>Job details</b></font></center>" );
+ Row2( "<b>Job ID:</b>", $Job_ID ); 
+ Row2( "<b>Application:</b>", $JobSpecs -> application ); 
+ Row2( "<b>State:</b>", $JobSpecs -> state ); 
+ Row2( "<b>State time stamp:</b>", gmdate( "j M Y G:i", $JobSpecs -> state_time_stamp ) ); 
+ Row2( "<b>Owners:</b>", $JobSpecs -> owners ); 
+ Row2( "<b>Read access:</b>", $JobSpecs -> read_access ); 
+ Row2( "<b>Target resources:</b>", $JobSpecs -> target_resources ); 
+ Row2( "<b>Job specifics:</b>", $JobSpecs -> job_specifics ); 
+ Row2( "<b>Input:</b>", $JobSpecs -> input ); 
+ Row2( "<b>Output:</b>", $JobSpecs -> output ); 
+ End_Table();
+
+ echo "<br><a href=qdel.php?job_id=$JobSpecs->job_id>Abort or Delete this job</a>\n";
+ echo "<br><a href=qstat.php>Show job list</a>\n"; 
+
+}
+else
+{
+
+ // check if state is set in request... otherwise default to any...
+ $State = "any";
+ if( isset( $_GET[ "state" ] ) )
+  $State = $_GET[ "state" ];
+ else
+  if( isset( $_POST[ "state" ] ) )
+   $State = $_POST[ "state" ];
+ $State = mysql_escape_string( $State );
+
+ // check if application is set in request... otherwise default to any...
+ $Application = "any";
+ if( isset( $_GET[ "application" ] ) )
+  $Application = $_GET[ "application" ];
+ else
+  if( isset( $_POST[ "application" ] ) )
+   $Application = $_POST[ "application" ];
+ $Application = mysql_escape_string( $Application );
+
+ // check if application is set in request... otherwise default to 0...
+ $Start = "0";
+ if( isset( $_GET[ "start" ] ) )
+  $Start = $_GET[ "start" ];
+ else
+  if( isset( $_POST[ "start" ] ) )
+   $Start = $_POST[ "start" ];
+ if( !is_numeric( $Start ) ) Exit_With_Text( "ERROR: Start not a number" );
+
+ // check if application is set in request... otherwise default to a nice number...
+ $Limit = "4098";
+ if( isset( $_GET[ "limit" ] ) )
+  $Limit = $_GET[ "limit" ];
+ else
+  if( isset( $_POST[ "limit" ] ) )
+   $Limit = $_POST[ "limit" ];
+ if( !is_numeric( $Limit ) ) Exit_With_Text( "ERROR: Limit not a number" );
+
+ Start_Table();
+ Row1( "<center><font color='green' size='4'><b>Leiden Grid Infrastructure basic interface QSTAT at ".gmdate( "j M Y G:i", time() )." UTC</b></font></center>" );
+ Row2( "<b>This project server:</b>", Get_Server_URL() ); 
+ Row2( "<b>Project master server:</b>", Get_Master_Server_URL() ); 
+ Row2( "<b>User:</b>", $User ); 
+ Row2( "<b>Groups:</b>", $Groups ); 
+ Row2( "<b>Project:</b>", $Project ); 
+ Row2( "<b>Application:</b>", $Application ); 
+ Row2( "<b>State:</b>", $State ); 
+ Row2( "<b>Start index:</b>", $Start ); 
+ Row2( "<b>Index limit:</b>", $Limit ); 
+
+ // build the query to execute...
+ $PossibleJobOwnersArray = CommaSeparatedField2Array( $User.", ".$Groups.", any", "," );
+ $Query = Interface_Make_Query_For_Work_For_Owners( $PossibleJobOwnersArray, $State, $Application, $Start, $Limit );
+
+ // do the query and get number of jobs to list...
+ $QueryResult = mysql_query( $Query );
+ $Number = mysql_num_rows( $QueryResult );
+ 
+ Row2( "<b>Number of jobs listed:</b>", $Number ); 
+
+ Row5( "<center><font color='green' size='4'><b>job id</b></font></center>", "<center><font color='green' size='4'><b>job state</b></font></center>", "<center><font color='green' size='4'><b>target resources</b></font></center>", "<center><font color='green' size='4'><b>application</b></font></center>", "<center><font color='green' size='4'><b>time stamp</b></font></center>" );
+
+ for( $i = 1; $i <= $Number; $i++ )
+ {
+  $Job = mysql_fetch_object( $QueryResult );
+  Row5( "<center><a href=qstat.php?job_id=$Job->job_id>$Job->job_id</a></center>", "<center>$Job->state</center>", "<center>$Job->target_resources</center>", "<center>$Job->application</center>", "<center>".gmdate( "j M Y G:i", $Job->state_time_stamp )."</center>" );
+ }
+
+ mysql_free_result( $QueryResult ); 
+
+ End_Table();
+}
+
 Page_Tail();
 
 ?>
