166fb414fd2fcc7af0bb2c453535207ce6f74c58
Mark Somers <m.somers@chem.leidenuniv.nl>
2008-10-12 17:35:48 +0000

    Updated repository API and added example put cgi script...
    git-svn-id: https://gliteui.wks.gorlaeus.net/svn/LGI/trunk@641 ce53c53d-4636-0410-80b1-9429a1ee4427

--- repository/repository_content.php
+++ repository/repository_content.php
@@ -1,5 +1,23 @@
 <?php
 
+// []--------------------------------------------------------[]
+//  |               repository_content.php                   |
+// []--------------------------------------------------------[]
+//  |                                                        |
+//  | AUTHOR:     M.F.Somers                                 |
+//  | VERSION:    1.00, August 2008.                         |
+//  | USE:        Simple API to list repository content...   |
+//  |                                                        |
+// []--------------------------------------------------------[]
+//
+//
+// Copyright (C) 2008 M.F. Somers, Theoretical Chemistry Group, Leiden University
+//
+// This is free software; you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation.
+//
+// http://www.gnu.org/licenses/gpl.txt
+
   if( isset( $_POST[ "repository" ] ) )
    $Respository = $_POST[ "repository" ];
 
