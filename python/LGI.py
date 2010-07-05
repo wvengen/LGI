bc82f5ca9eacb8a045410e7fab3695a9bafc5962
Mark Somers <m.somers@chem.leidenuniv.nl>
2010-07-05 13:26:47 +0000

    Didn't work...
    git-svn-id: https://gliteui.wks.gorlaeus.net/svn/LGI/trunk@942 ce53c53d-4636-0410-80b1-9429a1ee4427

--- python/LGI.py
+++ python/LGI.py
@@ -155,7 +155,6 @@ class LGI_Client:
 				File.write( Data );
 				Data = Response.read( 4096 );
 			File.close();
-			Response.close();
 
 	# if the class was setup with a repository URL, use this method to upload some files
 	def UpLoadFiles( self, Files ):
@@ -171,7 +170,6 @@ class LGI_Client:
 				Data = File.read( 4096 );
 			File.close();
 			Response = self._Connection.getresponse();
-			Response.close();
 	
 	# if the class was setup with a repository URL, use this method to delete some files
 	def DeleteFiles( self, Files ):
@@ -180,7 +178,6 @@ class LGI_Client:
 			BaseFileName = FileName[ FileName.rfind( '/' ) + 1 : len( FileName ) ];
 			self._Connection.request( "DELETE", self._URL + "/" + BaseFileName );
 			Response = self._Connection.getresponse();
-			Response.close();
 
 	# close the connection
 	def Close( self ):
@@ -229,9 +226,7 @@ class LGI_Client:
 			self._Connection.request( "POST", self._Path + API, Body, Headers );
 	
 		Response = self._Connection.getresponse();
-		Data = Response.read();
-		Response.close();
-		return( self.__NodesToDict( xml.dom.minidom.parseString( Data ) ) );
+		return( self.__NodesToDict( xml.dom.minidom.parseString( Response.read() ) ) );
 
 	class __NotATextNode:
 		pass;
